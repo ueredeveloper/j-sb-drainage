bcec822b4dda3dd8787af278e942a36ffcd52320
ueredeveloper <ueredeveloper@gmail.com>
2024-06-04 13:26:05 -0300

    adiciona diagrama de classe

---
+++ src/main/resources/uml-diagrams/js-drainage-diagrams.bat
@@ -0,0 +1,2 @@
+java -jar "C:\workspace\plantuml\plantuml-1.2023.13.jar" "C:\workspace\j-sb-drainage\src\main\resources\uml-diagrams\class-diagram.uml" -tpng -o "C:\workspace\j-sb-drainage\src\main\resources\uml-diagrams\uml-class-diagram.png"
+
