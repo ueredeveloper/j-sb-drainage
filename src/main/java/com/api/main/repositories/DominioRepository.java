664fbfa0befc19c1eba342e87b02693e420e229e
ueredeveloper <ueredeveloper@gmail.com>
2024-08-29 09:50:19 -0300

    fix: melhora query de pesquisa de tabelas dominio

--- src/main/java/com/api/main/repositories/DominioRepository.java
+++ src/main/java/com/api/main/repositories/DominioRepository.java
@@ -19,6 +19,12 @@ public interface DominioRepository extends JpaRepository<TipoInterferenciaModel,
 	@Query(value = "SELECT 'TipoAto', ta.id AS id, ta.descricao AS descricao FROM TIPO_ATO ta "
 			+ "UNION ALL SELECT 'TipoInterferencia', ti.id AS id, ti.descricao AS descricao FROM TIPO_INTERFERENCIA ti "
 			+ "UNION All SELECT 'TipoOutorga', to.id AS id, to.descricao AS descricao FROM TIPO_OUTORGA to "
+			+ "UNION All SELECT 'SubtipoOutorga', so.id AS id, so.descricao AS descricao FROM SUBTIPO_OUTORGA so "
+			+ "UNION All SELECT 'Estado', e.id AS id, e.descricao AS descricao FROM ESTADO e "
+			+ "UNION All SELECT 'SituacaoProcesso', sp.id AS id, sp.descricao AS descricao FROM SITUACAO_PROCESSO sp "
+			+ "UNION All SELECT 'Subsistema', s.id AS id, s.descricao AS descricao FROM SUBSISTEMA s "
+			+ "UNION All SELECT 'TipoPoco', tp.id AS id, tp.descricao AS descricao FROM TIPO_POCO tp "
+			+ "UNION All SELECT 'LocalCaptacao', lc.id AS id, lc.descricao AS descricao FROM LOCAL_CAPTACAO lc "
 			, nativeQuery = true)
 	List<Object[]> listAll();
 	
