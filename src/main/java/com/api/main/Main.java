33bb9c1180357121ec4f2b89645d9aefd22360ca
Fabrício José Barrozo <fabricio.barrozo@adasa.df.gov.br>
2025-01-17 08:49:45 -0300

    adicionar leitura .env

--- src/main/java/com/api/main/Main.java
+++ src/main/java/com/api/main/Main.java
@@ -5,12 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @RestController
 @SpringBootApplication
 public class Main {
 
 	
 	public static void main(String... args) {
+		// Captura variáveis de ambiente no arquivo .env
+		Dotenv dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .load();
+				// Make environment variables available
+				dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+				);
+				
 		SpringApplication.run(Main.class, args);
 	}
 
